5afd413f5f3fffc6d613aef1bd4953aec2fa5a4a
mysteriousreb1 <C.G.H.entreprise.directeur@gmail.com>
2026-09-25 15:25:04 +0200

    Update APP_SCRIPT_URL to new endpoint

--- rt-auth.js
+++ rt-auth.js
@@ -12,7 +12,7 @@
    sans ouvrir l'application.
    ===================================================== */
 
-const APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwIB962NoMwZjdv_CN4T3QQzmUpV24bNhYQ0pIy9qcZ0fXN984AzwGyBU2n2tHbheN_/exec";
+const APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyi-iJfUyRyN39R9vknQTyvgLeCMA3QzLtsD5N4XVwpJTMtsXMsUSsrsrqDXLi8DI1t/exec";
 const API_TIMEOUT_MS = 60000;   // Apps Script peut mettre ~1 min au tout premier appel (démarrage à froid)
 const TOKEN_KEY = "rt_session_token";
 const EMAIL_KEY = "rt_session_email";
